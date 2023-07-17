79b6a4229ab345450abdef126aa90f5ca48822cb
azadei <azadei@code.edu.az>
2023-07-17 14:50:31 +0400

    1

---
+++ 6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+//ex 6
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] DaysofWeek = { "Monday", "Tuesday", "Wednesday", "Thurusday", "Friday", "Saturday", "Sunday" };
+
+        var days = from Week in DaysofWeek
+
+                   select Week;
+
+        foreach (var Week in days)
+
+        {
+            Console.WriteLine(Week);
+        }
+
+
+    }
+}
